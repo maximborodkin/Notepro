14de609d29f2b4bcf74090cdf4d4c44a65d8ac9f
maximborodkin <maxim.borodkin1999@yandex.ru>
2021-12-10 18:38:19 +0300

    fix timeout preference

--- app/src/main/java/ru/modemastudio/notepro/persistence/PreferencesManager.kt
+++ app/src/main/java/ru/modemastudio/notepro/persistence/PreferencesManager.kt
@@ -15,13 +15,17 @@ class PreferencesManager @Inject constructor(context: Context) {
 
     val enableAutodelete = preferences.getBoolean(enableAutodeleteKey, false)
 
-    val autodeleteTimeout: Long = when(preferences.getInt(autodeleteTimeoutKey, 6)) {
-        1 -> 3_600_000 // Hour
-        2 -> 86_400_000 // Day
-        3 -> 604_800_000 // Week
-        4 -> 2_629_746_000 // Month
-        5 -> 15_778_476_000 // Six months
-        else -> 31_556_952_000 // Year
+    // Returns milliseconds of selected timeout
+    val autodeleteTimeout: Long = with(context) {
+        when (preferences.getString(autodeleteTimeoutKey, null)
+        ) {
+            getString(R.string.autodelete_timeout_one_hour) -> 3_600_000
+            getString(R.string.autodelete_timeout_one_day) -> 86_400_000
+            getString(R.string.autodelete_timeout_one_week) -> 604_800_000
+            getString(R.string.autodelete_timeout_one_month) -> 2_629_746_000
+            getString(R.string.autodelete_timeout_six_months) -> 15_778_476_000
+            else -> 31_556_952_000 // Year
+        }
     }
 
     val darkTheme = preferences.getBoolean(darkThemeKey, false)
